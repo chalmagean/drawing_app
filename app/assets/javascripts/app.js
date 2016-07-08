640026c31dd03789b833da786b12baad32eed075
Cezar Halmagean <cezar@mixandgo.com>
2016-07-08 11:57:12 +0300

    Paint clicks on canvas

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      points: [],
+      lines: []
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   componentDidMount() {
     const context = this._canvas.getContext('2d');
     this.paint(context);
@@ -9,20 +20,44 @@ export default class App extends Component {
 
   componentDidUpdate() {
     const context = this._canvas.getContext('2d');
-    context.clearRect(0, 0, 200, 200);
     this.paint(context);
   }
 
+  handleClick(e) {
+    const mouseX = e.pageX - this._canvas.offsetLeft;
+    const mouseY = e.pageY - this._canvas.offsetTop;
+
+    this.addPoint(mouseX, mouseY);
+  }
+
+  addPoint(x, y) {
+    const pointXY = [x, y];
+
+    this.setState({ points: this.state.points.concat([pointXY]) });
+  }
+
+  paintPoints(context) {
+    const points = this.state.points;
+
+    points.map(point => {
+      context.fillRect(point[0], point[1], 4, 4);
+    });
+  }
+
   paint(context) {
-    context.save();
-    context.translate(100, 100);
-    context.rotate(this.props.rotation, 100, 100);
-    context.fillStyle = '#F00';
-    context.fillRect(-50, -50, 100, 100);
-    context.restore();
+    // Clear the canvas
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    // Paint the points
+    // TODO: can this be done in an immutable way?
+   this.paintPoints(context);
   }
 
   render() {
-    return <canvas width={800} height={600} ref={(c) => this._canvas = c} />;
+    return <canvas
+      onClick={this.handleClick}
+      width={800}
+      height={600}
+      ref={(c) => this._canvas = c} />;
   }
 }
