e655e4fb28a40c8aaab3ecc965bdae45930ffe46
Cezar Halmagean <cezar@mixandgo.com>
2016-07-08 21:26:57 +0300

    Paint line drawings

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,11 +6,16 @@ export default class App extends Component {
     super(props);
 
     this.state = {
+      dragging: false,
+      currentLine: [],
       points: [],
       lines: []
     };
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleMouseClick = this.handleMouseClick.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
   }
 
   componentDidMount() {
@@ -23,17 +28,44 @@ export default class App extends Component {
     this.paint(context);
   }
 
-  handleClick(e) {
+  handleMouseClick(e) {
     const mouseX = e.pageX - this._canvas.offsetLeft;
     const mouseY = e.pageY - this._canvas.offsetTop;
 
     this.addPoint(mouseX, mouseY);
   }
 
+  handleMouseDown(e) {
+    this.setState({ dragging: true });
+  }
+
+  handleMouseUp(e) {
+    this.setState({
+      dragging: false,
+      lines: this.state.lines.concat([this.state.currentLine]),
+      currentLine: []
+    });
+  }
+
+  handleMouseMove(e) {
+    const mouseX = e.pageX - this._canvas.offsetLeft;
+    const mouseY = e.pageY - this._canvas.offsetTop;
+
+    if (this.state.dragging) {
+      this.addLineNode(mouseX, mouseY);
+    }
+  }
+
   addPoint(x, y) {
     const pointXY = [x, y];
 
     this.setState({ points: this.state.points.concat([pointXY]) });
+  }
+
+  addLineNode(x, y) {
+    const pointXY = [x, y];
+
+    this.setState({ currentLine: this.state.currentLine.concat([pointXY]) });
   }
 
   paintPoints(context) {
@@ -44,6 +76,28 @@ export default class App extends Component {
     });
   }
 
+  paintLines(context) {
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 2;
+
+    this.state.lines.map(line => {
+      context.beginPath();
+
+      line.map((node, index) => {
+        if (index === 0) {
+          // Beginning of the line
+          let firstNode = line[0];
+          context.moveTo(firstNode[0], firstNode[1]);
+        } else {
+          context.lineTo(node[0], node[1]);
+        }
+      });
+
+      context.stroke();
+    });
+  }
+
   paint(context) {
     // Clear the canvas
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
@@ -51,11 +105,15 @@ export default class App extends Component {
     // Paint the points
     // TODO: can this be done in an immutable way?
    this.paintPoints(context);
+   this.paintLines(context);
   }
 
   render() {
     return <canvas
-      onClick={this.handleClick}
+      onClick={this.handleMouseClick}
+      onMouseDown={this.handleMouseDown}
+      onMouseUp={this.handleMouseUp}
+      onMouseMove={this.handleMouseMove}
       width={800}
       height={600}
       ref={(c) => this._canvas = c} />;
